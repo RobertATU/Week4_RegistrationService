672c89428280eaf9448df5ac98fc78f803451dff
G00395612@atu.ie <G00395612@atu.ie>
2023-10-11 10:30:38 +0100

    Initial Commit

---
+++ src/main/java/ie/atu/week4_registrationservice/Week4RegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.week4_registrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week4RegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week4RegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/week4_registrationservice/Week4RegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week4_registrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4RegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
