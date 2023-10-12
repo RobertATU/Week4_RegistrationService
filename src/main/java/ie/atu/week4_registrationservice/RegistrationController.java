51289d7edaabba52024de4629f49649eb5b225d2
Robert <g00395612@atu.ie>
2023-10-12 20:53:16 +0100

    Added function for receiving data

--- src/main/java/ie/atu/week4_registrationservice/RegistrationController.java
+++ src/main/java/ie/atu/week4_registrationservice/RegistrationController.java
@@ -1,11 +1,15 @@
 package ie.atu.week4_registrationservice;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RegistrationController {
-    public void regRequests(){
-
+    @PostMapping("/confirm")
+    public String regRequests(@RequestBody UserDetails userDetails) {
+        String confirmMessage = String.format("Received details for %s with email %s", userDetails.getName(),userDetails.getEmail());
+        return confirmMessage;
     }
 
 }
