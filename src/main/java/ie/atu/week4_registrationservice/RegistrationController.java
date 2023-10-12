9de9b138d6b5eb7e44021779df2085f2f02a4128
Robert <g00395612@atu.ie>
2023-10-12 20:23:26 +0100

    User Details

---
+++ src/main/java/ie/atu/week4_registrationservice/RegistrationController.java
@@ -0,0 +1,11 @@
+package ie.atu.week4_registrationservice;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+    public void regRequests(){
+
+    }
+
+}
